e692ddb8e1a5dcd316d63ff8ff182d4baca8118e
Leandro Peres <leandroperes@protonmail.com>
2024-07-30 17:52:58 -0300

    ➕ Added spdlog

---
+++ cmake/Import/spdlog.cmake
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------
+# spdlog
+# --------------------------------------------------------------------
+find_package(spdlog CONFIG REQUIRED)
+list(APPEND LIBS spdlog::spdlog)
+#target_link_libraries(main PRIVATE spdlog::spdlog_header_only)
+

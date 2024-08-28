81dd8e3a8f9bf369d56646c7d6dd27f491c2660c
Leandro Peres <leandroperes@protonmail.com>
2024-08-28 17:23:13 -0300

    👷 Added Emscripten barrier

--- cmake/Import/SDL.cmake
+++ cmake/Import/SDL.cmake
@@ -1,17 +1,13 @@
 # --------------------------------------------------------------------
 # SDL Configuration
 # --------------------------------------------------------------------
-set(SDL_LIBC
-    ON
-    CACHE BOOL "" FORCE)
-set(SDL_TEST
-    OFF
-    CACHE BOOL "" FORCE)
-set(SDL2_DISABLE_INSTALL
-    OFF
-    CACHE BOOL "" FORCE)
+set(SDL_LIBC ON CACHE BOOL "" FORCE)
+set(SDL_TEST OFF CACHE BOOL "" FORCE)
 
 message(STATUS "Using LuGL's SDL")
+
 find_package(SDL2 CONFIG REQUIRED)
-list(APPEND LIBS $<TARGET_NAME_IF_EXISTS:SDL2::SDL2main>
-     $<IF:$<TARGET_EXISTS:SDL2::SDL2>,SDL2::SDL2,SDL2::SDL2-static>)
+list(APPEND LIBS
+$<TARGET_NAME_IF_EXISTS:SDL2::SDL2main>
+$<IF:$<TARGET_EXISTS:SDL2::SDL2>,SDL2::SDL2,SDL2::SDL2-static>
+)
